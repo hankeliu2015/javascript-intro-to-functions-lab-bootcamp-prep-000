ac8523065c5ec769176a27d534079554d1a81429
Hank Liu <hanke.liu@gmail.com>
2018-07-27 16:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     console.log("YES INDEED")
   } else if (string.toLowerCase === string) {
-    console.log("I can't hear you")
+    console.log("I can\'t hear you")
   } else if (string === "I love you, Grandma") {
     console.log("I love you, too")
   } 
