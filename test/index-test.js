c0a9ef79f70ceb5087cd658450c957b3677596b7
Hank Liu <hanke.liu@gmail.com>
2018-07-27 15:23:06 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
-function shout(string) {
-  return
-  string.toUpperCase()
-  }
+// function shout(string) {
+//   return
+//   string.toUpperCase()
+//   }
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
