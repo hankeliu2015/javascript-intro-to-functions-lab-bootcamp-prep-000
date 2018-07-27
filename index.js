c9306c3d8e1956dca34a149de47c634d335d00b5
Hank Liu <hanke.liu@gmail.com>
2018-07-27 17:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-
-swtich () {
+swtich (string) {
   
 }
+// function sayHiToGrandma(string) {
+
   // if (string === string.toLowerCase()) {
   //   console.log("I can\'t hear you!")
   // }
